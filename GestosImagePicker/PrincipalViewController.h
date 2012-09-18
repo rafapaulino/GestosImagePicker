0946b127ac6e144a87b405887621320e6bbf6e04
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-18 09:24:14 -0300

    Initial Commit

---
+++ GestosImagePicker/PrincipalViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrincipalViewController.h
+//  GestosImagePicker
+//
+//  Created by Rafael Brigagão Paulino on 18/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrincipalViewController : UIViewController
+
+@end
